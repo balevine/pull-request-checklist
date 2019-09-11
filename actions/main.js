3c84b35719e5161f32392cb88e3b993c145998b3
Brian <balevine@gmail.com>
2019-09-11 14:21:59 -0400

    text clarity and format

--- actions/main.js
+++ actions/main.js
@@ -10,13 +10,14 @@ async function run() {
 
   // create an issue comment with a merge checklist
   const body = `Thanks for opening a pull request! Before merging this pull request, please check off the following items:  
-    -[ ] Tests are written and passing.  
-    -[ ] Documentation has been written or updated (internal and external).  
-    -[ ] If the feature has a user-facing component, make sure it is backward compatible or that affected users have been notified of the change.  
-    -[ ] If possible, solicit feedback from other team members. For workflow changes, a walkthrough/demo is often helpful.  
-    -[ ] If possible, the branch has been branch-deployed and tested in production (you've sent some data through it and inspected logs for errors or anomalies).
+
+-[ ] Tests are written and passing.  
+-[ ] Documentation has been written or updated (internal and external).  
+-[ ] Solicit feedback from other team members. For workflow changes, a walkthrough/demo is often helpful.  
+-[ ] The branch has been branch-deployed and tested in production (you've sent some data through it and inspected logs for errors or anomalies).  
+-[ ] For user-facing components and features, make sure it is backward compatible (e.g. backfilled database data) or that affected users have been notified of the change.  
     
-    For checklist items that are not applicable, you can edit this comment and delete those items.`
+_For checklist items that are not applicable or possible, you can edit this comment and delete those items._`
 
   try {
     return octokit.issues.createComment({
