1c04ed33f66993721057dcdbe59da27fc27ebe0c
Brian <balevine@gmail.com>
2019-09-11 20:25:59 +0200

    spaces

--- actions/main.js
+++ actions/main.js
@@ -11,11 +11,11 @@ async function run() {
   // create an issue comment with a merge checklist
   const body = `Thanks for opening a pull request! Before merging this pull request, please check off the following items:  
 
--[ ] Tests are written and passing.  
--[ ] Documentation has been written or updated (internal and external).  
--[ ] Solicit feedback from other team members. For workflow changes, a walkthrough/demo is often helpful.  
--[ ] The branch has been branch-deployed and tested in production (you've sent some data through it and inspected logs for errors or anomalies).  
--[ ] For user-facing components and features, make sure it is backward compatible (e.g. backfilled database data) or that affected users have been notified of the change.  
+- [ ] Tests are written and passing.  
+- [ ] Documentation has been written or updated (internal and external).  
+- [ ] Solicit feedback from other team members. For workflow changes, a walkthrough/demo is often helpful.  
+- [ ] The branch has been branch-deployed and tested in production (you've sent some data through it and inspected logs for errors or anomalies).  
+- [ ] For user-facing components and features, make sure it is backward compatible (e.g. backfilled database data) or that affected users have been notified of the change.  
     
 _For checklist items that are not applicable or possible, you can edit this comment and delete those items._`
 
